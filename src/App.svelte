05f585c7aece801d8d2bef551af13ebeba465385
Gage Everett <rgeverett34@gmail.com>
2022-07-25 23:06:43 -0500

    input added

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang='ts'>
 
+let ans = 1
+
 let dialog = [
 	{
 		question: {
@@ -15,12 +17,57 @@ let dialog = [
 
 console.log(dialog)
 
+function getAnswer() {
+	let badAnswer = document.getElementById('input-answer-bad')
+	let goodAnswer = document.getElementById('input-answer-good')
+	console.log(badAnswer)
+	console.log(goodAnswer)
+}
+
 </script>
 
-{#each dialog as dia} 
-	{dia.question.ques}
-{/each}
+<div>
+	<p>
+		{dialog[0].question.ques}
+	</p>
+</div>
 
-<style>
+<div>
+	<form>
+		<label>
+			<input 
+				type='radio'
+				value={dialog[0].answers.good}
+				bind:group={ans}
+				id='input-answer-bad'
+			/>
+			<p class="good-answer">
+				{dialog[0].answers.good}
+			</p>
+		</label>
+		<label>
+			<input 
+				type='radio'
+				value={dialog[0].answers.bad}
+				bind:group={ans}
+				id='input-answer-good'
+			/>
+			<p class="bad-answer">
+				{dialog[0].answers.bad}
+			</p>
+		</label>
+	</form>
+	<button 
+			on:click|once={getAnswer}
+		>
+			Continue
+		</button>
+</div>
+
+<style lang='scss'>
+
+	.bad-answer, .good-answer {
+		display: inline-block;
+	}
 	
 </style>
