ffbc10241934f9da4cabf2720ae8b35426f3e964
Gage Everett <rgeverett34@gmail.com>
2022-07-25 22:26:42 -0500

    dialog added

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,25 @@
-<script>
+<script lang='ts'>
+
+let dialog = [
+	{
+		question: {
+			ques: "How are you?"
+		},
+
+		answers: {
+			bad: 'not pog',
+			good: 'pog'
+		}
+	}
+]
+
+console.log(dialog)
 
 </script>
 
-
+{#each dialog as dia} 
+	{dia.question.ques}
+{/each}
 
 <style>
 	
