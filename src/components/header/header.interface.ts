4119a3feedc018abb3eed271c62f75e4f44dc143
Gage Everett <rgeverett34@gmail.com>
2022-10-25 20:35:49 -0500

    header update

--- src/components/header/header.interface.ts
+++ src/components/header/header.interface.ts
@@ -1,12 +1,7 @@
 export interface Header {
-    Links: Link[]
-}
-
-interface Link {
     href: string,
     title: string,
     src: string,
     alt: string, 
     style: string, 
-
 }
--- src/components/header/header.svelte
+++ src/components/header/header.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 
     import type {Header} from './header.interface';
-    export let links: Header;
+    //export let links: Header[];
     export let customstyle: string = '';
     export let customclass: string = '';
     export let orientation: string = '';
-
+    export let links: any = [
+        {
+            title: 'hello'
+        },
+        {
+            title: 'hello'
+        }
+    ]
     
 
 </script>
@@ -15,7 +22,9 @@
 {#if links}
 
 <div class="header-wrapper">
-    
+    {#each links as link}
+        <p>{link.title}</p>
+    {/each}
 </div>
     
 {/if}
